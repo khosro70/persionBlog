dc4753397d3fd58021e657c692ee45bb834c6519
khosro moradi <moradikhosro89@gmail.com>
2025-04-23 12:37:50 +0330

    some styles weere changed

--- src/app/blogs/_components/PostInteraction.tsx
+++ src/app/blogs/_components/PostInteraction.tsx
@@ -41,7 +41,7 @@ const PostInteraction: React.FC<PostInteractionType> = ({ post }) => {
   };
 
   return (
-    <div className="flex items-center gap-x-4">
+    <div className="flex items-center gap-x-4 d">
       <ButtonIcon variant="secondary">
         <ChatBubbleOvalLeftEllipsisIcon />
         <span>{toPersianDigits(post?.commentsCount)}</span>
